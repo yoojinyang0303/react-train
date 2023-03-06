d559e2795e55ee5440674959d210eadf056bb984
yjyang <yoojinyang0303@gmail.com>
2023-03-07 00:28:51 +0900

    feat: react, react-dom 세팅

---
+++ lecture24/train03/src/components/FollowButton.js
@@ -0,0 +1,54 @@
+// React Component!
+// Follow <-> following 동작을 하는 버튼
+
+// starter code
+function FollowButton() {
+  const [following, setFollowing] = React.useState(false);
+
+  const commonBtnStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: "12px",
+    right: "16px",
+    width: "100px",
+    height: "36px",
+    borderRadius: "9999px",
+    fontWeight: "bold",
+  };
+
+  const followBtnStyle = {
+    ...commonBtnStyle,
+    color: "white",
+    backgroundColor: "black",
+  };
+
+  const followingBtnStyle = {
+    ...commonBtnStyle,
+    border: "1px solid #cfd9de",
+    color: "white",
+    backgroundColor: "black",
+  };
+
+  // Javascript -> JSX
+  //   return React.createElement(
+  //     "div",
+  //     {
+  //       onClick: () => setFollowing(!following),
+  //       style: following ? followingBtnStyle : followBtnStyle,
+  //     },
+  //     following ? "following" : "Follow"
+  //   );
+  return (
+    <div
+      onClick={() => setFollowing(!following)}
+      style={following ? followingBtnStyle : followingBtnStyle}
+    >
+      {following ? "following" : "Follow"}
+    </div>
+  );
+}
+// 아래 두 줄의 코드는 첫 단계에서 HTML 페이지에 추가했던 <div> 태그를 찾아주고 그 안에 "Following"이라는 내용의 React 컴포넌트를 추가한다.
+const domContainer = document.querySelector("#follow_button_container");
+ReactDOM.render(React.createElement(FollowButton), domContainer);

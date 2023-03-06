b672774519c381ddb90c7105b592af7849ec9d06
yjyang <yoojinyang0303@gmail.com>
2023-03-07 00:55:39 +0900

    feat: Webpack 세팅하기

--- lecture24/train03/src/components/FollowButton.js
+++ lecture24/train03/src/components/FollowButton.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 // React Component!
 // Follow <-> following 동작을 하는 버튼
 
@@ -49,6 +51,5 @@ function FollowButton() {
     </div>
   );
 }
-// 아래 두 줄의 코드는 첫 단계에서 HTML 페이지에 추가했던 <div> 태그를 찾아주고 그 안에 "Following"이라는 내용의 React 컴포넌트를 추가한다.
-const domContainer = document.querySelector("#follow_button_container");
-ReactDOM.render(React.createElement(FollowButton), domContainer);
+
+export default FollowButton;

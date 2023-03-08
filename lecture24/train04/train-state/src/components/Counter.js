0767bc045fa90f1b90fe5a1a632709ff6b292eb4
yjyang <yoojinyang0303@gmail.com>
2023-03-09 00:32:24 +0900

    원시타입 state 다루기 연습

--- lecture24/train04/train-state/src/components/Counter.js
+++ lecture24/train04/train-state/src/components/Counter.js
@@ -4,11 +4,41 @@ import React, { useState } from "react";
 export default function Counter() {
   // useState의 네이밍 컨밴션:: [state - 값, setState - 값을 변경할 때 쓰는 함수]
   const [count, setCount] = useState(0);
+  //show and hide - boolean으로
+  const [show, setShow] = useState(true);
+  // +, -, * 연산 - string
+  const operators = ["+", "-", "*"];
+  const [operator, setOperator] = useState(operators[0]);
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button
+        onClick={() => {
+          let result;
+          if (operator === "+") {
+            result = count + 1;
+          }
+          if (operator === "-") {
+            result = count - 1;
+          }
+          if (operator === "*") {
+            result = count * 1;
+          }
+          setCount(result);
+        }}
+      >
+        {operator}1
+      </button>
+      <button onClick={() => setShow(!show)}>Show & Hide</button>
+      <button
+        onClick={() => {
+          const idx = Math.floor(Math.random() * operators.length);
+          setOperator(operators[idx]);
+        }}
+      >
+        Change Operator
+      </button>
       <br />
-      Counter: {count}
+      {show && `Counter: ${count}`}
     </div>
   );
 }

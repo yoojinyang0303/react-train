59a31c97c068bf5725a735bd99c358a77a4e1c40
yjyang <yoojinyang0303@gmail.com>
2023-03-11 14:30:43 +0900

    feat: form 다중입력 다루기- 다중입력 자체를 하나의 useState 객체로

--- lecture24/train09/form-app/src/components/SimpleForm.js
+++ lecture24/train09/form-app/src/components/SimpleForm.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 
 export default function SimpleForm() {
-  const [nickname, setNickname] = useState("");
-  const [password, setPassword] = useState("");
+  //   const [nickname, setNickname] = useState("");
+  //   const [password, setPassword] = useState("");
+
+  // 다중 입력 자체를 하나의 useState로
+  const [userInputs, setUserInputs] = useState({
+    nickname: "",
+    password: "",
+  });
+
   const handleChange = (e) => {
     // e.target.name === "nickname" ? return setNickname(e.target.value) : return setPassword(e.target.value);
-    if (e.target.name === "nickname") {
-      return setNickname(e.target.value);
-    } else {
-      return setPassword(e.target.value);
-    }
+
+    setUserInputs({ ...userInputs, [e.target.name]: e.target.value });
+
+    // if (e.target.name === "nickname") {
+    //   return setNickname(e.target.value);
+    // } else {
+    //   return setPassword(e.target.value);
+    // }
   };
 
   // e.target.value의 쓰임?
@@ -17,6 +27,7 @@ export default function SimpleForm() {
   const handleSubmit = (e) => {
     e.preventDefault(); // event 기본 동작 제거
     // 서버 요청 + a
+    const { nickname, password } = userInputs;
     alert(`nickname: ${nickname}, password: ${password}`);
   };
 
@@ -26,12 +37,17 @@ export default function SimpleForm() {
       <input
         type="text"
         name="nickname"
-        value={nickname}
+        value={userInputs.nickname}
         onChange={handleChange}
       />
       <br />
       <label>패스워드: </label>
-      <input type="text" name="PW" value={password} onChange={handleChange} />
+      <input
+        type="text"
+        name="password"
+        value={userInputs.password}
+        onChange={handleChange}
+      />
       <input type="submit" value="제출" />
     </form>
   );

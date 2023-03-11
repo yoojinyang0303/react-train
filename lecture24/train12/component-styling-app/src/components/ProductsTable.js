ccfe9e713566b4ef3cd7ce76983e475275be8efd
yjyang <yoojinyang0303@gmail.com>
2023-03-12 04:33:27 +0900

    feat: styled-components 실습

--- lecture24/train12/component-styling-app/src/components/ProductsTable.js
+++ lecture24/train12/component-styling-app/src/components/ProductsTable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as S from "./ProductsTable.style";
 
 export default function ProductsTable(props) {
   const { category, items, inStockOnly } = props;
@@ -9,16 +10,18 @@ export default function ProductsTable(props) {
   return (
     <>
       <tr>
-        <td style={{ fontWeight: "bold" }}>{category}</td>
+        <S.Category>{category}</S.Category>
       </tr>
       {filteredItems.map((item, idx) => (
         <tr key={idx}>
-          <td style={item.stocked ? { color: "black" } : { color: "red" }}>
-            {item.name}
-          </td>
+          <ProductName stocked={item.stocked}>{item.name}</ProductName>
           <td>{item.price}</td>
         </tr>
       ))}
     </>
   );
 }
+
+const ProductName = td`
+  color: ${(props) => (props.stocked ? "black" : "red")};
+`;

3ce3095db79695e62c6c65ec5956e6f0fb759cb1
yjyang <yoojinyang0303@gmail.com>
2023-03-11 16:13:34 +0900

    feat: ref - mutable object 연습

--- lecture24/train10/ref-app/src/components/CatParent.js
+++ lecture24/train10/ref-app/src/components/CatParent.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Cat from "./Cat";
 
 // 목표:: Cat.js 안에 있는 img 사이즈 알고 싶다!
@@ -7,14 +7,24 @@ export default function CatParent() {
   console.log("부모 컴포넌트 CatParent");
   console.log(catRef);
 
+  const ageRef = useRef(1);
+  const [state, setState] = useState(1);
+  console.log(ageRef.current);
+
   return (
     <div>
       <h4> 고양이가 세상을 구한다 ️</h4>
       <div>
         <Cat a={"a"} ref={catRef} />
+        <h3>고양이 나이: {ageRef.current}살</h3>
+        <h4>고양이 상태값: {state}</h4>
         <button onClick={() => alert(catRef.current.height)}>
           고양이 사진 크기 알고시펑
         </button>
+        <button onClick={() => (ageRef.current = ageRef.current + 1)}>
+          고양이 나이++
+        </button>
+        <button onClick={() => setState(state + 1)}>상태++</button>
       </div>
     </div>
   );

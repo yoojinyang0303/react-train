b2571de24ee6b15dab22434f5ed5b302ba1e8bbd
yjyang <yoojinyang0303@gmail.com>
2023-03-11 14:06:36 +0900

    feat: form 제어 컴포넌트

--- lecture24/train09/form-app/src/components/SimpleForm.js
+++ lecture24/train09/form-app/src/components/SimpleForm.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SimpleForm() {
+  const [nickname, setNickname] = useState("initial");
   const handleChange = (e) => {
-    console.log(e.target.value);
+    setNickname(e.target.value);
+  };
+
+  // e.target.value의 쓰임?
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // event 기본 동작 제거
+    // 서버 요청 + a
+    alert(nickname);
   };
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <label>닉네임: </label>
-      <input type="text" name="nickname" onChange={handleChange} />
-    </>
+      <input
+        type="text"
+        name="nickname"
+        value={nickname}
+        onChange={handleChange}
+      />
+      <input type="submit" value="제출" />
+    </form>
   );
 }

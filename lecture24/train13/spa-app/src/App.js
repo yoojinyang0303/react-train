cdabb63f21bcbe1b3f6e5b0791a1f373e3da304e
yjyang <yoojinyang0303@gmail.com>
2023-03-12 13:06:19 +0900

    feat: useNavigate로 내비게이션

--- lecture24/train13/spa-app/src/App.js
+++ lecture24/train13/spa-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import MainPage from "./components/MainPage";
--- lecture24/train13/spa-app/src/components/ReactDocPage.js
+++ lecture24/train13/spa-app/src/components/ReactDocPage.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function ReactDocPage() {
   // docID: 1, 2, 3, ...
   const params = useParams();
-  console.log(params);
-  return <div>ReactDocPage ##{params.docId}</div>;
+  console.log(params); // url parameter 잘 불러오는지 확인용 로그 찍기
+
+  const navigate = useNavigate();
+  return (
+    <>
+      <h5 onClick={() => navigate("/")}>logo</h5>
+      <div>ReactDocPage ##{params.docId}</div>
+    </>
+  );
 }
